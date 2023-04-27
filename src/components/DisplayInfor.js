0cfc427394ffbfdb558c926e8f3a7dc0a38c5f32
giatha0 <thao8c89@gmail.com>
2023-04-27 14:57:25 +0700

    conditional output

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,22 +1,40 @@
 import React from "react";
 
 class DisplayInfor extends React.Component {
-    render() {
 
+    state = {
+        isShowListUser: true
+    }
+    handleShowHide = () => {
+        this.setState({
+            isShowListUser: !this.state.isShowListUser
+        })
+    }
+    render() {
         // destructuring array/object
         const { listUsers } = this.props; //object
         // props => viết tắt properties
         return (
             <div>
-                {listUsers.map((user) => {
-                    return (
-                        <div key={user.id}>
-                            <div>My name's {user.name} </div>
-                            <div>My age's {user.age} </div>
-                            <hr />
-                        </div>
-                    )
-                })}
+                <div>
+                    <span onClick={() => { this.handleShowHide() }}>
+
+                        {this.state.isShowListUser === true ? "Hide list users:" : "Show list users:"}
+                    </span>
+                </div>
+                {this.state.isShowListUser &&
+                    <div>
+                        {listUsers.map((user) => {
+                            return (
+                                <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+                                    <div>My name's {user.name} </div>
+                                    <div>My age's {user.age} </div>
+                                    <hr />
+                                </div>
+                            )
+                        })}
+                    </div>
+                }
             </div>
         )
     }
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -9,14 +9,14 @@ class MyComponent extends React.Component {
 
     state = {
         listUser: [
-            { id: 1, name: "Thao", age: 23 },
-            { id: 2, name: "Huy", age: 20 },
-            { id: 3, name: "Nam", age: 36 }
+            { id: 1, name: "Thao", age: "16" },
+            { id: 2, name: "Huy", age: "26" },
+            { id: 3, name: "Nam", age: "69" }
         ]
     }
     // jsx
     render() {
-        const myInfor = ['ab', 'ac', 'v'];
+        // const myInfor = ['ab', 'ac', 'v']
         return (
             <div>
                 <UserInfor />
