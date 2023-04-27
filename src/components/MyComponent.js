8fae2a2cfc5bef675141bd40bcb276b149f03de4
giatha0 <thao8c89@gmail.com>
2023-04-27 13:55:56 +0700

    props

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -3,14 +3,20 @@
 
 import React from "react";
 import UserInfor from "./UserInfor";
+import DisplayInfor from "./DisplayInfor";
 
 class MyComponent extends React.Component {
 
     // jsx
     render() {
+        const myInfor = ['ab', 'ac', 'v'];
         return (
             <div>
                 <UserInfor />
+                <br /><br />
+                <DisplayInfor name="Thao" age="23" />
+                <hr />
+                <DisplayInfor name="Duong" age={20} myInfor={myInfor} />
             </div>
         );
     }
