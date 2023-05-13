df0f68e04963e951b54e70f41414f2b0a9c2d094
giatha0 <thao8c89@gmail.com>
2023-05-13 18:38:16 +0700

    index routes

---
+++ src/components/Home/HomePage.js
@@ -0,0 +1,9 @@
+const HomePage = (props) => {
+    return (
+        <div className="homepage-container">
+            homep page component
+        </div>
+    )
+}
+
+export default HomePage;
--- src/index.js
+++ src/index.js
@@ -12,6 +12,8 @@ import {
 } from 'react-router-dom';
 import User from './components/User/User';
 import Admin from './components/Admin/Admin';
+import HomePage from './components/Home/HomePage';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
@@ -19,6 +21,7 @@ root.render(
     <BrowserRouter >
       <Routes>
         <Route path='/' element={<App />} >
+          <Route index element={<HomePage />} />
           <Route path='/users' element={<User />} />
           <Route path='/admins' element={<Admin />} />
         </Route>
