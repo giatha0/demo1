1e60b2fa89c06263e53dd7378a99ea21132435e1
giatha0 <thao8c89@gmail.com>
2023-05-13 18:58:59 +0700

    design homepage

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,7 +1,11 @@
+import videoHomePage from '../../assets/video-homepage.mp4';
+
 const HomePage = (props) => {
     return (
         <div className="homepage-container">
-            homep page component
+            <video autoPlay muted loop >
+                <source src={videoHomePage} type="video/mp4" />
+            </video>
         </div>
     )
 }
