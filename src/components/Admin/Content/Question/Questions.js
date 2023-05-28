39a11323e9df8e3789cfd75a870be3232457bb7c
giatha0 <thao8c89@gmail.com>
2023-05-28 18:58:49 +0700

    api add new questions/answers

--- src/components/Admin/Content/Question/Questions.js
+++ src/components/Admin/Content/Question/Questions.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Select from 'react-select';
 import './Questions.scss';
 import { HiPlusCircle, HiMinusCircle } from 'react-icons/hi';
@@ -6,18 +6,11 @@ import { RiImageAddFill } from 'react-icons/ri'
 import { v4 as uuidv4 } from 'uuid';
 import _ from 'lodash';
 import Lightbox from "react-awesome-lightbox";
+import { getAllQuizForAdmin, postCreateNewQuestionForQuiz, postCreateNewAnswerForQuiz } from '../../../../services/apiService';
 
 
 
 const Questions = () => {
-
-    const options = [
-        { value: 'chocolate', label: 'Chocolate' },
-        { value: 'strawberry', label: 'Strawberry' },
-        { value: 'vanilla', label: 'Vanilla' },
-    ];
-
-    const [selectedQuiz, setSelectedQuiz] = useState({});
 
     const [questions, setQuestions] = useState(
         [
@@ -42,6 +35,26 @@ const Questions = () => {
         title: '',
         url: ''
     })
+    const [listQuiz, setListQuiz] = useState([]);
+    const [selectedQuiz, setSelectedQuiz] = useState({});
+
+    useEffect(() => {
+        fetchQuiz();
+    }, [])
+
+    const fetchQuiz = async () => {
+
+        let res = await getAllQuizForAdmin();
+        if (res && res.EC === 0) {
+            let newQuiz = res.DT.map(item => {
+                return {
+                    value: item.id,
+                    label: `${item.id} - ${item.description}`
+                }
+            })
+            setListQuiz(newQuiz)
+        }
+    }
 
     const handleAddRemoveQuestion = (type, id) => {
         if (type === 'ADD') {
@@ -151,8 +164,29 @@ const Questions = () => {
         }
     }
 
-    const handleSubmitQuestionForQuiz = () => {
-        console.log('questions: ', questions);
+    const handleSubmitQuestionForQuiz = async () => {
+
+        //todo
+        //validate
+
+
+        //submit questions
+        await Promise.all(questions.map(async (question) => {
+            const q = await postCreateNewQuestionForQuiz(
+                +selectedQuiz.value,
+                question.description,
+                question.imageFile
+            );
+            //submit answers
+            await Promise.all(question.answers.map(async (answer) => {
+                await postCreateNewAnswerForQuiz(
+                    answer.description, answer.isCorrect, q.DT.id
+                )
+            }))
+
+        }));
+
+        //submit answers
     }
 
 
@@ -168,7 +202,7 @@ const Questions = () => {
                     <Select
                         value={selectedQuiz}
                         onChange={setSelectedQuiz}
-                        options={options}
+                        options={listQuiz}
                     />
                 </div>
                 <div className='mt-3 mb-2'>
--- src/services/apiService.js
+++ src/services/apiService.js
@@ -90,10 +90,26 @@ const putUpdateQuizForAdmin = (id, description, name, difficulty, quizImage) => 
     return axios.put('api/v1/quiz', data);
 }
 
+const postCreateNewQuestionForQuiz = (quiz_id, description, image) => {
+    const data = new FormData();
+    data.append('quiz_id', quiz_id);
+    data.append('description', description);
+    data.append('questionImage', image);
+
+    return axios.post('api/v1/question', data);
+}
+
+const postCreateNewAnswerForQuiz = (description, correct_answer, question_id) => {
+    return axios.post('api/v1/answer', {
+        description, correct_answer, question_id
+    });
+}
+
 export {
     postCreateUserNewUser, getAllUsers, putUpdateUser,
     deleteUser, getUserWithPaginate, postLogin,
     postRegister, getQuizByUser, getDataQuiz,
     postSubmitQuiz, postCreateNewQuiz, getAllQuizForAdmin,
-    deleteQuizForAdmin, putUpdateQuizForAdmin
+    deleteQuizForAdmin, putUpdateQuizForAdmin, postCreateNewQuestionForQuiz,
+    postCreateNewAnswerForQuiz
 }
